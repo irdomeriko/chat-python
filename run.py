007875b76d65e3201691e122165812f544591865
irdomeriko <irdomeriko@gmail.com>
2025-03-28 19:38:21 +0100

    Creating run.py

---
+++ run.py
@@ -0,0 +1,15 @@
+import os
+print("""
+Escribe un número para elegir una opción:
+1. Servidor
+2. Cliente 
+3. Salir
+""")
+respuesta = int(input())
+
+if respuesta == 1:
+    os.system("python3 server.py")
+elif respuesta == 2:
+    os.system("python3 client.py")
+elif respuesta == 3:
+    exit()
